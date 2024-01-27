1df81043bdfc26170c1a0308d76eff07fe49510e
Your Name <chase.allen@dcmail.ca>
2024-01-27 01:29:42 -0500

    2nd Commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,37 +1,18 @@
 "use strict";
 
 
-(function(){
+(function () {
+    //Create a function for click events with variables for different buttons and logic
 
-    function DisplayHomePage(){
+
+    function DisplayHomePage() {
         console.log("Called DisplayHomePage()");
-        let AboutUsButton = document.getElementById("AboutUsBtn")
-
-        AboutUsButton.addEventListener("click", function() {
-           location.href = "about.html";
-        });
-
-        let MainContent = document.getElementsByTagName("main")[0];
-        let MainParagraph = document.createElement("p");
-
-        MainParagraph.setAttribute("id", "MainParagraph" );
-        MainParagraph.setAttribute("class", "mt-3" );
-        MainParagraph.textContent = "This is my first paragraph";
-        MainContent.appendChild(MainParagraph);
+        let ExploreBtn = document.getElementById("ExploreBtn");
+        let AboutUsSection = document.getElementById("AboutUsSection");
 
 
-        let FirstString = "This is";
-        let SecondString = `${FirstString} the main paragraph`;
-        MainParagraph.textContent = SecondString;
-        MainContent.appendChild(MainParagraph);
-
-        let DocumentBody = document.body;
-        let Article = document.createElement("article");
-        let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is my article paragraph</p>`;
-        Article.setAttribute("class", "container");
-        Article.innerHTML = ArticleParagraph;
-        DocumentBody.appendChild(Article);
     }
+
 
     function DisplayProductPage(){
         console.log("Called DisplayProductPage()");
@@ -120,5 +101,6 @@
 
     }
     window.addEventListener("load", Start);
+
 
 })()
